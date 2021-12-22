330087bda9cb9ec79ef1f363d28846209f3541c8
Gayane25 <gayabennu@gmail.com>
2021-12-22 21:06:40 +0400

    feat:solved with getter setter

--- getSet.js
+++ getSet.js
@@ -1,2 +1,17 @@
 'use strict';
 //Write an object with field name.
+
+const obj = {
+  _name: [],
+  get name() {
+    return obj._name;
+  },
+  set name(value) {
+    value = value.split(' ');
+    for (let i = 0; i < value.length; i++) {
+      obj._name.push([value[i], value[i].length]);
+    }
+  },
+};
+obj.name = 'Hovhannes, Sona';
+console.log(obj.name);
