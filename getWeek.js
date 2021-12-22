3e2c3234e466a289d83cfbaa5b6fee741d8a9702
Gayane25 <gayabennu@gmail.com>
2021-12-23 00:00:11 +0400

    feat: added strict mode and ticket in comment

---
+++ getWeek.js
@@ -0,0 +1,2 @@
+'use strict';
+/*Write a function that, given a date (in the format MM/DD/YYYY), returns the day of the week as a string. Each day name must be one of the following strings: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", or "Saturday".*/
